1811bf813396ba349a2055809ced21b997fc786d
hail <1144062115@qq.com>
2020-08-15 23:06:29 +0800

    test

---
+++ test2.cpp
@@ -0,0 +1,16 @@
+//测试
+#include<bits/stdc++.h>
+using namespace std;
+const int maxn=1e6+9;
+int d[maxn];//点i的度数
+int x[maxn],y[maxn];
+int id[maxn],col[1040][1040];
+vector<int>g[maxn];
+bool vis[maxn];
+int match[maxn];
+int n,m;
+int main ()
+{
+    
+    return 0;
+}
